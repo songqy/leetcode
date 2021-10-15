bde01ec5fa1be0ea78a0214faba1886f7a8d84ff
songqy <songqy3@126.com>
2021-10-15 18:33:25 +0800

    263

---
+++ 263.js
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode.cn id=263 lang=javascript
+ *
+ * [263] 丑数
+ *
+ * https://leetcode-cn.com/problems/ugly-number/description/
+ *
+ * algorithms
+ * Easy (51.52%)
+ * Likes:    278
+ * Dislikes: 0
+ * Total Accepted:    106.5K
+ * Total Submissions: 206.8K
+ * Testcase Example:  '6'
+ *
+ * 给你一个整数 n ，请你判断 n 是否为 丑数 。如果是，返回 true ；否则，返回 false 。
+ *
+ * 丑数 就是只包含质因数 2、3 和/或 5 的正整数。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：n = 6
+ * 输出：true
+ * 解释：6 = 2 × 3
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：n = 8
+ * 输出：true
+ * 解释：8 = 2 × 2 × 2
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：n = 14
+ * 输出：false
+ * 解释：14 不是丑数，因为它包含了另外一个质因数 7 。
+ *
+ *
+ * 示例 4：
+ *
+ *
+ * 输入：n = 1
+ * 输出：true
+ * 解释：1 通常被视为丑数。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * -2^31
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function (n) {
+  if (n <= 0) return false;
+  while (n % 2 === 0) {
+    n = n / 2;
+  }
+  while (n % 3 === 0) {
+    n = n / 3;
+  }
+  while (n % 5 === 0) {
+    n = n / 5;
+  }
+  return n === 1;
+};
+// @lc code=end
+
+// console.log(isUgly(1));
+// console.log(isUgly(6));
+console.log(isUgly(8));
+// console.log(isUgly(14));
+// console.log(isUgly(-2147483648)); // false
+// console.log(isUgly(59049));
+// console.log(isUgly(282429536481));
